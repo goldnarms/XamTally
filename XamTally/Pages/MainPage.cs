70274658423c4e2971e752b9a7fb9b437b8a7341
Arnstein Johansen <arnstej@gmail.com>
2014-10-24 00:00:59 +0200

    Added mainpage

---
+++ XamTally/Pages/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Runtime;
+
+namespace XamTally.Pages
+{
+    internal class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Content = BuildContent();
+        }
+
+        private View BuildContent()
+        {
+            var grid = new Grid()
+            {
+                BackgroundColor = Color.Transparent,
+                ColumnDefinitions = new ColumnDefinitionCollection()
+                {
+                    new ColumnDefinition { Width = GridLength.Auto },
+                    new ColumnDefinition { }
+                },
+                RowDefinitions = new RowDefinitionCollection()
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition()
+                }
+            };
+            var stackLayout = new StackLayout();
+            stackLayout.Children.Add(new Label {
+                Text = "Tap to increase tally",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.White
+            });
+            grid.Children.Add(stackLayout,1, 1);
+            return grid;
+        }
+    }
+}
